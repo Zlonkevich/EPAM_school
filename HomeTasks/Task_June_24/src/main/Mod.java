c975053e62ada749c2a0e45a8b072bbd9792d539
Fedor <zlonkevich89@gmail.com>
2017-06-28 23:53:20 +0300

    Done

--- HomeTasks/Task_June_24/src/main/Mod.java
+++ HomeTasks/Task_June_24/src/main/Mod.java
@@ -3,22 +3,12 @@ package main;
 public class Mod {
 
     public static int mod(int a, int b) {
-        int sign = 1;
-        if (a == 0)
-            return 0;
-        else {
-            sign = a / Math.abs(a) * b / Math.abs(b);
-        }
+        int c = a / b;
+        int ost = a - c * b;
 
-        int result = a;
+//        int ost = a % b;
 
-        if (Math.abs(a) >= Math.abs(b)) {
-            while (Math.abs(result) >= Math.abs(b)) {
-                result = Math.abs(result) - Math.abs(b);
-            }
-            return result * sign;
-        } else return a;
-
+        return ost;
 
     }
 }
