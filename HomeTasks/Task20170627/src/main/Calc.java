6fd0f4bdebe2fc15a360a293f9dc32b62492d138
Fedor <zlonkevich89@gmail.com>
2017-06-29 20:44:21 +0300

    Correct negative input devision

--- HomeTasks/Task20170627/src/main/Calc.java
+++ HomeTasks/Task20170627/src/main/Calc.java
@@ -6,17 +6,22 @@ public class Calc {
     public static String toBin(int a) {
         StringBuilder res = new StringBuilder();
         int b;
+        int sign = a;
 
-        if(a < 0){
-            res.append('-');
-        }else if(a == 0){
+        if (a < 0) {
+            a = a * (-1);
+        } else if (a == 0) {
             return "0";
         }
 
-        while ( a != 0){
+        while (a != 0) {
             b = a % 2;
             res.append(b);
             a = a / 2;
+        }
+
+        if (sign < 0) {
+            res.append("-");
         }
         res.reverse();
         return res.toString();
@@ -24,56 +29,71 @@ public class Calc {
 
     public static String toOcto(int a) {
         StringBuilder res = new StringBuilder();
+        int sign = a;
         int b;
 
-        if(a < 0){
-            res.append('-');
-        }else if(a == 0){
+        if (a < 0) {
+            a = a * (-1);
+        } else if (a == 0) {
             return "0";
         }
 
 
-
-        while(a != 0){
+        while (a != 0) {
             b = a % 8;
             res.append(b);
             a = a / 8;
         }
+
+        if (sign < 0) {
+            res.append("-");
+        }
+
         res.reverse();
         return res.toString();
     }
-
 
 
     public static String toHex(int a) {
         StringBuilder res = new StringBuilder();
         int b;
+        int sign = a;
 
-        if(a < 0){
-            res.append('-');
-        }else if(a == 0){
+        if (a < 0) {
+            a = a * (-1);
+        } else if (a == 0) {
             return "0";
         }
 
-        while(a != 0){
+        while (a != 0) {
             b = a % 16;
             res.append(hex(b));
             a = a / 16;
+        }
+
+        if (sign < 0) {
+            res.append("-");
         }
         res.reverse();
         return res.toString();
     }
 
 
-    private static String hex(int i){
+    private static String hex(int i) {
         switch (i) {
-            case 10: return "a";
-            case 11: return "b";
-            case 12: return "c";
-            case 13: return "d";
-            case 14: return "e";
-            case 15: return "f";
+            case 10:
+                return "a";
+            case 11:
+                return "b";
+            case 12:
+                return "c";
+            case 13:
+                return "d";
+            case 14:
+                return "e";
+            case 15:
+                return "f";
         }
-        return ((Integer)i).toString();
+        return ((Integer) i).toString();
     }
 }
--- HomeTasks/Task20170627/src/main/Starter.java
+++ HomeTasks/Task20170627/src/main/Starter.java
@@ -3,16 +3,18 @@ package main;
 public class Starter {
     public static void main(String[] args) {
 
-        System.out.println(Calc.toBin(123));
-        System.out.println(Calc.toHex(123));
-        System.out.println(Calc.toOcto(123));
+        System.out.println(Calc.toBin(555));
+        System.out.println(Calc.toHex(555));
+        System.out.println(Calc.toOcto(555));
 
-        System.out.println(Calc.toBin(-123));
-        System.out.println(Calc.toHex(-123));
-        System.out.println(Calc.toOcto(-123));
+        System.out.println(Calc.toBin(-555));
+        System.out.println(Calc.toHex(-555));
+        System.out.println(Calc.toOcto(-555));
 
         System.out.println(Calc.toBin(0));
         System.out.println(Calc.toHex(0));
         System.out.println(Calc.toOcto(0));
+
+
     }
 }
